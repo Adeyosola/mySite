b5d97f1d2c6a1000231cbd9ee747366a621b113c
Adeyosola <adepojua@uwindsor.ca>
2017-05-11 16:06:09 -0400

    Added BootStrap for styling

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
+import { Jumbotron, Button } from 'react-bootstrap';
+import AppBar from './AppBar/AppBar.jsx';
 
 class App extends React.Component {
   render () {
-    return (
+    return(
     <div>
-      <p> Hello React!</p>
-      <AwesomeComponent />
+    <AppBar/>
+    <p> Hello React!</p>
+    <Jumbotron>
+    <h1>Hello, world!</h1>
+    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <p><Button bsStyle="primary">Learn more</Button></p>
+  </Jumbotron>
     </div>
-    );
+    ) ;
+
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public'); //represents the directory path of the bundle file output
-var APP_DIR = path.resolve(__dirname, 'src/client/app'); //holds the directory path of the React Application codebase
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: APP_DIR +'/index.jsx', //the entry file using which the bundling process starts (kind of like the main class in Java)
-  output: { //instructs webpack on what to do after the bundling is completed.
+  entry: APP_DIR + '/index.jsx',
+  output: {
     path: BUILD_DIR,
-    filename: 'bundle.js' //output the bundled file into this file in the specified directory
+    filename: 'bundle.js'
   },
   module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
-      }
-    ]
-  }
+   loaders : [
+     {
+       test : /\.jsx?/,
+       include : APP_DIR,
+       loader : 'babel-loader'
+     }
+   ]
+ }
 };
 
 module.exports = config;
